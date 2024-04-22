68e992507f36b075258bdb2c44e9bc8801ebcc5a
Lovro Grguric Mileusnic <lovrogm@gmail.com>
2024-04-22 12:27:49 +0200

    Add default UDS impl.

--- impl/config.py
+++ impl/config.py
@@ -1,7 +1,7 @@
-from scapy.layers.can import CAN_INV_FILTER
-
+# see https://scapy.readthedocs.io/en/latest/layers/automotive.html#native-cansocket
 CAN_FILTERS = [
-    {"can_id": 0x123, "can_mask": 0x7FF},
-    {"can_id": 0x100 | CAN_INV_FILTER, "can_mask": 0x7FF},
+    {"can_id": 0x456, "can_mask": 0x7FF},
 ]
+
+# receive and transmit arbitration IDs (ISO-TP address of the UDS server)
 UDS = dict(rx_id=0x100, tx_id=0x101)
--- impl/uds_handler.py
+++ impl/uds_handler.py
@@ -1,4 +1,4 @@
-from scapy.contrib.automotive.uds import UDS
+from scapy.contrib.automotive.uds import *
 
 from ecu_template.handler.uds.uds_handler import UDSHandler
 from ecu_template.model.ecu_model import ECUModel
@@ -11,4 +11,10 @@ class UDSHandlerImpl(UDSHandler):
         )
 
     def handle_msg(self, msg: UDS):
-        print("Received UDS message!")
+        match msg.payload:
+            case UDS_ER():
+                self.isotp.send(UDS() / UDS_ERPR(resetType=msg.resetType))
+            case UDS_DSC():
+                self.isotp.send(
+                    UDS() / UDS_DSCPR(diagnosticSessionType=msg.diagnosticSessionType)
+                )
